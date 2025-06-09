218ef55ba3a2f2067e2a30e8e9915d337ca35da6
Francisco Colmenero <franciscojcolmenero@outlook.com>
2025-06-08 23:24:40 -0500

    Hamburger menu bg less opacity & bg color to grey

--- vantageform-webapp/src/app/components/design/Header.jsx
+++ vantageform-webapp/src/app/components/design/Header.jsx
@@ -91,7 +91,7 @@ export const BackgroundBalls = React.memo(() => {
 export const HamburgerMenu = () => {
     return (
       <div className="absolute inset-0 pointer-events-none lg:hidden">
-        <div className="absolute inset-0 opacity-[.1]">
+        <div className="absolute inset-0 opacity-[.3]">
           <Image
             className="w-full h-full object-cover"
             src={background}
